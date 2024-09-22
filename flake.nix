75007b45acb13507ea9b7fb5da263a8a2699148e
affirm-bats-yodel <affirm-bats-yodel@duck.com>
2024-09-22 06:18:43 +0000

    chore: add flake

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = "A Nix Flake for Wiki Mulistream Index Parser";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem(system: 
+      let pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          nativeBuildInputs = with pkgs; [
+            go
+            golangci-lint
+            marksman
+          ];
+        };
+      }
+    );
+}
